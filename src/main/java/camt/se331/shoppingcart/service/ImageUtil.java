cd5a88c1985c96e434c8e5667189c53e77894398
panpanLittlepan <panpan.littlepan@gmail.com>
2016-04-03 21:08:43 +0700

    3.16 is done again

--- src/main/java/camt/se331/shoppingcart/service/ImageUtil.java
+++ src/main/java/camt/se331/shoppingcart/service/ImageUtil.java
@@ -1,7 +1,10 @@
 package camt.se331.shoppingcart.service;
 
 import camt.se331.shoppingcart.entity.Image;
+import org.imgscalr.Scalr;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,17 +25,22 @@ public class ImageUtil {
         return imageUtil;
     }
 
-    public static Image getImage(String resourcePath){
+    public static Image getImage(String resourcePath) throws IOException {
         Image image = new Image();
         ClassLoader classLoader = ImageUtil.getInstance().getClass().getClassLoader();
         File file = new File(classLoader.getResource(resourcePath).getFile());
-
+        BufferedImage originalImage= ImageIO.read(file);
+        BufferedImage target= Scalr.resize(originalImage,Scalr.Method.QUALITY,
+                200, 200);
         try {
 
             image.setFileName(file.getName());
             image.setContentType(Files.probeContentType(file.toPath()));
             FileInputStream fis = new FileInputStream(file);
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            ImageIO.write(target,"jpg",bos);
+
             byte[] buf = new byte[1024];
             for (int readNum; (readNum = fis.read(buf)) != -1;){
                 bos.write(buf,0,readNum);
